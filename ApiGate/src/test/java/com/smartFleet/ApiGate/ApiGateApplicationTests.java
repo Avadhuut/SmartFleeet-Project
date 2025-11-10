00042fa5add43cdc21d0f9734b4747d65e9274f9
Avadhuut <avadhutbhosale2001@gmail.com>
2025-11-10 17:35:17 +0530

    nothing

---
+++ ApiGate/src/test/java/com/smartFleet/ApiGate/ApiGateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smartFleet.ApiGate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
